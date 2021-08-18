228d416e51983a31e94d9eb7872d29aee05edc71
Pasgen-Manley <pasgen95@gmail.com>
2021-08-18 16:18:57 +0800

    connected server.js to mysql and emterprise database

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'Sweggy4Beggy21!',
+    database: 'enterprise_db'
+  },
+  console.log('Connected to the enterprise_db database!')
+);
+
