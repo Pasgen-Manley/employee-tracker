d5bd213ce08bb57873341e01108ed53fb85884cd
Pasgen-Manley <pasgen95@gmail.com>
2021-08-20 12:04:55 +0800

    seeds inputted

--- db/seeds.sql
+++ db/seeds.sql
@@ -17,4 +17,12 @@ VALUES ('Sales Officer', 70000, 1),
        ('Human Resources Officer', 80000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Jeff', 'Milligan', )
+VALUES ('Jeff', 'Milligan', 1, 1),
+       ('Bill', 'Lee', 2, null),
+       ('Heaf', 'Rod', 3, 2),
+       ('Shane', 'Warne', 4, 2),
+       ('Josh', 'Jacobs' 5, 3),
+       ('Jason', 'Wicks', 6, 3),
+       ('Phillip', 'Leca', 7, 4),
+       ('Jin', 'Sakai', 8, 4),
+       ('Sam', 'Sung', 9, 5);
