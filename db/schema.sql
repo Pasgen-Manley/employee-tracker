6f40a3dc88065e17b001fbd28fb839ebcc7a33ef
Pasgen-Manley <pasgen95@gmail.com>
2021-08-20 11:39:07 +0800

    fixed spelling of last name

--- db/schema.sql
+++ db/schema.sql
@@ -21,7 +21,7 @@ DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
   id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
-  last_namee VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   role_id INT,
   manager_id INT
 );
